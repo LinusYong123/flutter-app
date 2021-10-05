fe23ee13539b126b9e7e7085b9eeb674bcedf6fe
FlutLab.io <>
2021-10-05 23:33:05 +0000

    Initial commit

---
+++ lib/pages/loading.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void getData() async {
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
+    var response = await http.get(url);
+    Map data = jsonDecode(response.body);
+    print(data);
+    print(data['title']);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Loading Screen...'),
+    ); // Scaffold
+  }
+}
