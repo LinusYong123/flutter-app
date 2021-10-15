b5969c33b9e6a3db13ce07fb36307a6afe3bcfac
susukacang <yongsoonfatt@gmail.com>
2021-10-14 17:55:11 -0700

    edit files

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
-import 'package:helloworld/services/world_time.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,6 +6,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  Map data = {};
+
   @override
   void initState() {
     super.initState();
@@ -17,55 +17,63 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
 
-    Map data = ModalRoute.of(context).settings.arguments;
-    print(data);
+    data = ModalRoute.of(context).settings.arguments;
 
-
-    String bgImage = data['isDatetime'] ? 'day.png': 'night.png';
+    // set background image
+    String bgImage = data['isDaytime'] ? 'day.png' : 'night.png';
+    Color bgColor = data['isDaytime'] ? Colors.blue : Colors.indigo[700];
 
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
         child: Container(
           decoration: BoxDecoration(
             image: DecorationImage(
-              image: AssetImage('assets/night.png'),
+              image: AssetImage('assets/$bgImage'),
               fit: BoxFit.cover,
-            ),
+            )
           ),
           child: Padding(
             padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
             child: Column(
               children: <Widget>[
                 FlatButton.icon(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/location');
-                    },
-                    icon: Icon(Icons.edit_location),
-                    label: Text('Edit Location')),
-                    SizedBox(height: 20.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                            data['location'],
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              letterSpacing: 2.0,
-                          ),
-                        ),
-                      ],
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  icon: Icon(
+                    Icons.edit_location,
+                    color: Colors.grey[300],
+                  ),
+                  label: Text(
+                    'Edit Location',
+                    style: TextStyle(
+                      color: Colors.grey[300],
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                            data['time'],
-                            style: TextStyle(
-                              fontSize: 66.0,
-                          ),
-                        ),
-                      ],
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                        color: Colors.white,
+                      ),
                     ),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontSize: 66.0,
+                    color: Colors.white
+                  )
+                ),
               ],
             ),
           ),
